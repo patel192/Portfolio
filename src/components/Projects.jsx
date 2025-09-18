b1add3edd640f4f5546e0be0d517b248473c925b
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-09-18 22:02:33 +0530

    Url correction

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ export const Projects = () => {
     id: 1,
     title: "Expense Manager",
     desc: "A full-stack app to track daily expenses with charts.",
-    image: "public/cost-management-rgb-color-icon-vector.jpg",
+    image: "/cost-management-rgb-color-icon-vector.jpg",
     tech: ["React", "Node.js", "MongoDB"],
     github: "https://github.com/yourname/expense-manager",
     demo: "https://expense-manager-frontend-sw2e.vercel.app"
@@ -16,7 +16,7 @@ export const Projects = () => {
     title: "SkillHub",
     desc: "An e-learning platform with progress tracking.",
     image:
-      "public/students-using-e-learning-platform-video-laptop-graduation-cap-online-education-teaching-concept-bright-vibrant-violet-135958890.webp",
+      "/students-using-e-learning-platform-video-laptop-graduation-cap-online-education-teaching-concept-bright-vibrant-violet-135958890.webp",
     tech: ["React", "Express", "PostgreSQL"],
     github: "https://github.com/yourname/skillhub",
     demo: "https://skill-hub-aq91.vercel.app"
